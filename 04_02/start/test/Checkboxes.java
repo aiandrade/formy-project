759a8ef9ec6f63dfe1d16b3ff6f019061e53105c
aiandrade <anaisa0987@gmail.com>
2019-06-17 16:54:10 -0700

    Initial commt

---
+++ 04_02/start/test/Checkboxes.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Checkboxes {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/anais/Downloads/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/checkbox");
+
+        WebElement checkBox1 = driver.findElement(By.id("checkbox-1"));
+        checkBox1.click();
+
+        WebElement checkBox2 = driver.findElement(By.id("checkbox-2"));
+        checkBox2.click();
+
+        WebElement checkBox3 = driver.findElement(By.id("checkbox-3"));
+        checkBox3.click();
+
+        driver.quit();
+    }
+}
